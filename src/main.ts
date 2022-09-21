375e4293493882b8f9cca5ead1d6663b58b7a4a2
wangchaochuan <wangcci@enn.cn>
2022-09-21 18:34:35 +0800

    feat:init project

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+
+// const name = "";
+
+// const a = 11;
+// const b = 11;
+// if (a == b) {
+//   console.log(true);
+// }
+
+// console.info(name);
+// const name = "";
+// const name = '';
+
+createApp(App).mount('#app');
